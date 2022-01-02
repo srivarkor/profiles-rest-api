f9f41786ea7cc6da7e324f92a961a7650bf66805
srivarkor <srivardhanakorada@digiframe.org>
2022-01-02 23:57:06 +0530

    Add View Set and CRUD operations

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,56 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework import viewsets
 from profiles_api import serializers
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test ViewSet"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self,request):
+        """Return an Hello Message"""
+        a_viewset = [
+        'Uses actions (list, create, retrieve, update, partial_update)',
+        'Automatically maps to URLS using Routers',
+        'Provides more functionality with less code'
+        ]
+
+        return Response({'message': 'Hello!!', 'a_viewset': a_viewset})
+
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!!!'
+            return Response({'message': message})
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method':'GET'})
+
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method':'PUT'})
+
+
+    def partial_update(self, request, pk=None):
+        """Handle partial update of an object"""
+        return Response({'http_method':'PATCH'})
+
+
+    def destroy(self, request, pk=None):
+        """Handle deletion of an object via its ID"""
+        return Response({'http_method':'DELETE'})
+
 
 
 class HelloApiView(APIView):
