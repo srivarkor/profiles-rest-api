67292e05dc09d0c521af5c4412cb13a80cddf2cd
srivarkor <srivardhanakorada@digiframe.org>
2022-01-03 21:58:11 +0530

    Search Profile capability added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import viewsets,filters
 from profiles_api import serializers
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
@@ -14,6 +14,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
 
 
 class HelloViewSet(viewsets.ViewSet):
